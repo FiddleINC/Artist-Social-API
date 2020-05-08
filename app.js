9a6cde10a036acd5ce29c9509b3179380915bd6d
Prachurjya Biswas <biswasprachurjya@gmail.com>
2020-05-08 13:44:43 +0530

    Added home page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+var socialRouter = require('./routes/social');
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -15,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/socials', indexRouter);
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/socials', socialRouter);
 
 app.listen(3000, () => {
 	console.log('Listening at 3000');
--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,14 @@
 var express = require('express');
-var google = require('google');
-var cheerio = require('cheerio');
-var chalk = require('chalk');
+var fs = require('fs');
+var marked = require('marked');
+var showdown = require('showdown');
 var router = express.Router();
 
-var socials = {
-	facebook: '',
-	twitter: '',
-	instagram: '',
-	soundcloud: '',
-	spotify: '',
-	appleMusic: ''
-};
-
-var getSocials = (queryName, res) => {
-	google.resultsPerPage = 25;
-	google(queryName, function(err, response) {
-		console.log(chalk.bgMagenta.black(`Searching Socials for ${queryName} `));
-		if (err) console.error(err);
-		var $ = cheerio.load(response.body);
-		$('a').each((i, elem) => {
-			const link = $(elem).attr('href').replace('/url?q=', '').split('&')[0];
-			const domain = link
-				.replace('http://', '')
-				.replace('https://', '')
-				.replace('www.', '')
-				.split(/[/?#]/)[0]
-				.replace('.com', '');
-			switch (domain) {
-				case 'facebook':
-					if (socials.facebook == '') socials.facebook = link;
-					break;
-				case 'twitter':
-					if (socials.twitter == '') socials.twitter = link.split('%')[0];
-					break;
-				case 'instagram':
-					if (socials.instagram == '') socials.instagram = link.split('%')[0];
-					break;
-				case 'soundcloud':
-					if (socials.soundcloud == '') socials.soundcloud = link;
-					break;
-				case 'open.spotify':
-					if (socials.spotify == '') socials.spotify = link;
-					break;
-				case 'music.apple':
-					if (socials.appleMusic == '') socials.appleMusic = link;
-				default:
-					break;
-			}
-		});
-		res.json(socials);
-	});
-};
-
 router.get('/', (req, res) => {
-	getSocials(req.query.name, res);
-	socials = {
-		facebook: '',
-		twitter: '',
-		instagram: '',
-		soundcloud: '',
-		spotify: '',
-		appleMusic: ''
-	};
+	//res.send('This is a Artist Social API');
+	var readMe = fs.readFileSync('README.md', 'utf-8');
+	var markdownReadMe = marked(readMe);
+	res.send(markdownReadMe);
 });
 
 module.exports = router;
